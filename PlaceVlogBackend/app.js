f99ccf8ca24c62022d6d30a623d7cd3c71efd795
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-05-13 11:35:03 +0530

    Initial commit

---
+++ PlaceVlogBackend/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const HttpError = require("./models/http-error");
+
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
+
+app.use( '/api/places' , placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500).json({message: error.message || 'An unknown error occured!'});
+})
+
+mongoose
+.connect('mongodb+srv://mayank-user:dcHjWc5scDEzrqtc@pvcluster1.rshm41y.mongodb.net/placevlogs?retryWrites=true&w=majority&appName=PVCluster1')
+.then(() => {
+    app.listen(5000);
+})
+.catch(err => {
+    console.log(err);
+});
+
